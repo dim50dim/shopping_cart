cb176187c25c0d608b36e795fa7a4b37743e3ab6
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-24 19:17:13 +0200

    created three tsx files in pages folder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import Home from "./pages/Home"
+import Store from "./pages/Store"
+import About from "./pages/About"
 
 
 function App() {
@@ -8,7 +11,11 @@ function App() {
   return (
     <>
     <Container>
-        Hi
+        <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/store" element={<Store/>} />
+           <Route path="/about" element={<About/>} />
+        </Routes>
     </Container>
         
     </>
