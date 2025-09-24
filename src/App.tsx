3a3ac9e36147b2f70861646beb8bed5a46aa7779
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-24 19:18:46 +0200

    added style to Container. almost the same like in tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <Container>
+    <Container className="mb-4">
         <Routes>
            <Route path="/" element={<Home/>} />
            <Route path="/store" element={<Store/>} />
