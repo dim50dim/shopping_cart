b35c06477206002ef113a5cbb0b9ecc34edac8ab
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-25 17:40:39 +0200

    Cool feature NavLink and Nav from bootstrap. easy and smooth way to create navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom"
-import { Container } from "react-bootstrap"
+import { Container} from "react-bootstrap"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
 import About from "./pages/About"
-
+import Navbar from "./components/Navbar"
 
 function App() {
 
 
   return (
     <>
+    <Navbar/>
     <Container className="mb-4">
         <Routes>
            <Route path="/" element={<Home/>} />
