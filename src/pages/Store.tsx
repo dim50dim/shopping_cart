ea4fe5479512b857c39cabb9140827f6e928dccc
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-25 18:45:20 +0200

    in Store page - show up all data from item.json.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import storeItems from "../data/items.json"
+import { Col, Row } from 'react-bootstrap'
 const Store = () => {
   return (
     <div>
       <h1>Store</h1>
+       <Row>
+          {storeItems.map(item => (
+            <Col> {JSON.stringify(item)} </Col>
+          ))}
+    
+       </Row>
     </div>
   )
 }
