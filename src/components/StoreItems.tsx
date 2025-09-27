80e07438636c5ff616753ec6fddd234c3fe2b97b
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-27 10:59:32 +0200

    styled cartes, tailwindcss is definitely easier way to make css things.

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -14,8 +14,14 @@ const StoreItems = ({id,name,price,imgUrl}: StoreItemsProps) => {
            variant="top" 
            src={imgUrl}
            height="200px"
-           style={{objectfit : "cover"}}
+           style={{objectFit : "cover"}}
            />
+           <Card.Body className='d-flex flex-column'>
+            <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+                 <span className='fs-2'> {name} </span>
+                 <span className='ms-2 text-muted'> {price} </span>
+            </Card.Title>
+           </Card.Body>
       </Card>
     </div>
   )
