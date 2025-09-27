1986411417dc5b382489fdfb7de789059e496369
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-27 14:20:35 +0200

    obviously, styled with bootstrap is quite different than with tailwindcss

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 import { formatCurrency } from '../utilities/formatCurrency';
 type StoreItemsProps={
   id :number,
@@ -8,9 +8,10 @@ type StoreItemsProps={
   imgUrl : string,
 }
 const StoreItems = ({id,name,price,imgUrl}: StoreItemsProps) => {
+  const quantity = 0
   return (
     <div>
-      <Card>
+      <Card className='h-100'>
           <Card.Img
            variant="top" 
            src={imgUrl}
@@ -22,6 +23,11 @@ const StoreItems = ({id,name,price,imgUrl}: StoreItemsProps) => {
                  <span className='fs-2'> {name} </span>
                  <span className='ms-2 text-muted'> {formatCurrency(price)} </span>
             </Card.Title>
+            <div className='mt-auto'>
+              {quantity === 0 ?(
+                 <Button className='w-100'>  Add to Cart</Button>
+              ): null}
+            </div>
            </Card.Body>
       </Card>
     </div>
